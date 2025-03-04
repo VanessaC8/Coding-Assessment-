bf5cd2578192dcf98b3f3fff994c398287a083dc
VanessaC8 <vcastro5656@gmail.com>
2025-03-04 02:56:14 +0000

    finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Create a program that will take schedual of incoming spaceships, each with different sizes and docking time
 # requierments, and a map of available docking bays at a space station.
-
+import random 
 import dockingBays as db 
 
 # Function to print docking bays information
@@ -23,7 +23,7 @@ def main():
 # This function will take the schedule of the spaceship and list avalible bays based on the size of the ships
 def schedule_ships():
 # This string will save the bay 
-    incoming_ships = {Name:['ship_name'], Size:['size'], time:['arrival_time'], Exit:['departure_time']}
+    incoming_ships = random.choice(incoming_ships)
 # This if else statement will go through each size and tell you which size bays are available 
     if 'Size' == 'small' in db.dockingBays:
         print(f"Docking bays vailable are {{Bay:['bay_id'], Size:['size'], time:['arrival_time'}}.")
@@ -35,18 +35,18 @@ def schedule_ships():
 
 # This function will tell you which bays are aviable for the ships docking time window 
 def list_of_bays():
-    incoming_ships = {'Name': ['ship_name'], 'Size':['size'], 'time':['arrival_time'], 'exit':['departure_time']}
+    incoming_ships = random.choice(incoming_ships)
 #This if else statement will go though each time and find one that will be available for the ships docking window 
     if 
 
 
 # This function will assign ships a bay based on size and time 
 def assign_bays():
-    incoming_ships = {'Name': ['ship_name'], 'Size':['size'], 'time':['arrival_time'], 'exit':['departure_time']}
+    incoming_ships = random.choice(incoming_ships)
 # This if else statment will go through all the requierments to assign each ship a bay
-    if (size) & (time) in db.dockingBays:
+    if (size) & (time) in docking_bays:
         print(f"You are assinged bay {docking_bays}")
-    elif (size) & (time) in db.dockingBays:
+    elif (size) & (time) in dockign_bays:
         print(f"You are assinged bay {docking_bays}")
 
 
@@ -54,8 +54,8 @@ def assign_bays():
 
 # This function will give the amout of times for each ship so that they leave on time so they dont stay to long and take up space
 def optimized_time():
-    incoming_ships = {'Name': ['ship_name'], 'Size':['size'], 'time':['arrival_time'], 'exit':['departure_time']}
-    
+    incoming_ships = random.choice(incoming_ships)
+     
 
 
 
